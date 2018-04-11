778b835139c3aefde95002965315218d9c808791
Cathleen Wright <cathleenmwright@gmail.com>
2018-04-11 11:20:43 -0400

    Conditionals

---
+++ conditionals.php
@@ -0,0 +1,62 @@
+<?php
+  define("TITLE", "PHP Syntax")
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php echo TITLE ?></title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="container">
+    <h1><?php echo TITLE; ?></h1>
+
+    <?php
+      $foo = 99;
+      $bar = 75;
+      // https://www.w3schools.com/php/php_operators.asp
+      if ($foo > $bar) {
+        echo "$foo is greater than $bar with double quotes<br>";
+        echo '$foo is greater than $bar with single quotes';
+        echo "<hr>";
+      }
+
+      $currentlyListeningTo = "Bach";
+      if ($currentlyListeningTo == "Aerosmith") {
+        echo "Rock on! You are listening to $currentlyListeningTo";
+      } else {
+        echo "You are listening to $currentlyListeningTo";
+      }
+      echo "<hr>";
+      $favProgrammingLang = "Javascript";
+      if ($favProgrammingLang == "C++") {
+        echo "Your favorite programming language is $favProgrammingLang";
+      } else if ($favProgrammingLang == "PHP"){
+        echo "Your favorite programming language is $favProgrammingLang, which is the best choice.";
+      } else {
+        echo "You don't like PHP or C++";
+      }
+
+
+     ?>
+
+</div>
+
+    <!-- jQuery -->
+    <script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
+
+    <!-- Bootstrap JS -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  </body>
+</html>
